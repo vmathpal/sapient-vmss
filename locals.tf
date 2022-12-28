ffe7b9920499e7c9619eb1605ab3a3cd2a6a4884
vmathpal <40942280+vmathpal@users.noreply.github.com>
2022-12-28 13:31:23 +0530

    Create locals.tf

---
+++ locals.tf
@@ -0,0 +1,13 @@
+locals {
+  custom_data = <<EOF
+  #!/bin/bash
+  sudo yum upgrade -y
+  sudo yum check-update; sudo yum install -y gcc libffi-devel python-devel openssl-devel epel-release
+  sudo yum install -y python3-pip python3-wheel
+  LC_CTYPE=en_US.UTF-8
+  export LC_CTYPE 
+  sudo python3 -m pip install --upgrade --force-reinstall pip
+  python3 -m pip install setuptools_rust
+  python3 -m pip install ansible
+  EOF
+  }
