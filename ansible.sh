9f0d854478d82f493bf0a1bc72e1a45914ed5c6e
vmathpal <40942280+vmathpal@users.noreply.github.com>
2022-12-29 10:41:31 +0530

    updated azure cli installation

--- ansible.sh
+++ ansible.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 sudo yum upgrade -y &&
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[azure-cli]
+name=Azure CLI
+baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo
 sudo yum check-update; sudo yum install -y gcc libffi-devel python-devel openssl-devel epel-release
 sudo yum install -y python3-pip python3-wheel
 LC_CTYPE=en_US.UTF-8
 export LC_CTYPE 
 sudo python3 -m pip install --upgrade --force-reinstall pip
 python3 -m pip install setuptools_rust
+sudo yum install -y azure-cli
 python3 -m pip install ansible
